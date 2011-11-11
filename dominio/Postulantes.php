f16434a563b3259b8c56b95812681edd02b86762
Leandro <leojg2091@gmail.com>
2011-11-11 18:09:28 -0300

    Cree una carpeta dominio y dos clases Usuarios y su subclase Postulantes.

---
+++ dominio/Postulantes.php
@@ -0,0 +1,62 @@
+<?php
+
+	/**
+	 *  Clase encargada de representar los Usuarios Postulantes
+	 */
+	class Postulantes extends Usuarios  {
+		
+		private $sexo;
+		private $fNac;
+		private $localidad;
+		
+		function __construct($nom,$pass,$mail,$sexo,$fNac,$localidad) {
+			
+			parent:__construct($nom,$pass,$mail);
+			
+			$this->sexo=$sexo;
+			$this->localidad=$localidad;
+			$this->fNac=$fNac;
+			
+		 }
+		
+		function toString() {
+		
+		return  parent::toString()." Sexo: ".$this->sexo." Localidad: ".$this->localidad ;
+		
+		}
+		
+		function getSexo() {
+			return $this->sexo;
+		}
+		
+		function getLoc() {
+			return $this->localidado;
+		}
+		
+		function getFNac() {
+			return $this->fNacf;
+		}
+		
+		function setSexo($sex) {
+			
+			$this->sexo=$sex;	
+			
+		}
+		
+		function setLoc($loc) {
+			
+			$this->localidado=$loc;	
+			
+		}
+		
+		function setFNac($fnac) {
+			
+			$this->fNac=$fnac;	
+			
+	}
+			
+		
+	}
+
+
+?>
---
+++ dominio/Usuarios.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Representa los usuarios del sitio
+ */
+class Usuarios {
+	
+	private $nombre;
+	private $pass;
+	private $usrmail;
+	
+	function __construct($nom,$pass,$mail) {
+		$this->nombre=$nom;
+		$this->usrmail=$mail;
+		$this->pass=$pass;
+	}
+	
+	function toString() {
+		
+		return "Usuario: ". $this->nombre." EMail: ".$this->usrmail;
+		
+	}
+	
+	function getNom() {
+		return $this->nombre;
+	}
+	
+	function getMail() {
+		return $this->usrmail;
+	}
+	
+	function getPass() {
+		return $this->pass;
+	}
+	
+	function setNom($nom) {
+		
+		$this->nombre=$nom;	
+		
+	}
+	
+	function setMail($mail) {
+		
+		$this->usrmail=$mail;	
+		
+	}
+	
+	function setNom($pass) {
+		
+		$this->pass=$pass;	
+		
+	}
+			
+}
+?>
