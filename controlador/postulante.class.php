55e3ed7ce5affbe2d6c8952315a6702637bc1997
Leandro <leojg2091@gmail.com>
2011-11-14 16:04:49 -0300

    Borre el alta temporal que estaba al pedo.
    Hice el metodo de alta y de baja de postulantes, y uno para obtener los datos desde la tabla.

--- controlador/postulante.class.php
+++ controlador/postulante.class.php
@@ -5,8 +5,9 @@
 	 * Implementa Singleton Pattern
      */
     class Postulante{
+    	
       private static $instance;
-	  
+	  	  
       private function __construct($argument) {
       	//algo
       }
@@ -19,17 +20,84 @@
 		return self::$instance;
 	  }
 	  
-	  public function alta($value='') {
+	  public function alta($objP) {
 		
+		$arrP = obtener();
+		
+		foreach ($arrP as $key => $value) {
+		
+			if ($objP->getNom() != $key->getNom()) {
+				
+				$sentenciaSql = <<<SQL
+insert into postulante('nom','pass','mail','sexo','fNac','localidad')
+values($objP->getNom(),$objP->getPass(),
+$objP->getMail(),$objP->getSexo(),
+$objP->getFNac(),$objP->getLoc())
+SQL;
+
+				mysql_query($sentenciaSql);
+				break;
+			}
+		} 
 	  }
 	  
-	  public function baja($value='') {
+	  public function baja($objP) {
+		
+				$arrP = obtener();
+		
+		foreach ($arrP as $key => $value) {
+		
+			if ($objP->getNom() == $key->getNom()) {
+				
+				$sentenciaSql = <<<SQL
+delete from postulantes
+where nom=$objP->getNom() 
+SQL;
+
+				mysql_query($sentenciaSql);
+				break;
+				
+			}
+		}
 		
 	  }
 	  
 	  public function modificar($value='') {
 	  	
 	  }
+	  
+	  public function obtener() {
+	  	
+
+		
+			$sentenciaSql = <<<SQL
+select * from Postulantes
+SQL;
+
+			$resQuery = mysql_query($sentenciaSql);
+			
+			if ($resQuery) {
+				
+				$arrPost;
+				$cont = 0;
+				
+				while($dato = mysql_fetch_assoc($resQuery)) {
+					
+					$objP = $dato['nom'].",".$dato['pass'].",".$dato['mail'].",".
+					$dato['sexo'].",".$dato['fNac'].",".$dato['localidad'];
+					
+					$arrPost[$cont] = $objP;
+					$cont ++;
+					
+				}
+				
+				return $arrPost or die("No existen elementos");
+				
+			}
+			
+	  }
+	  
+	  	  
     }
     
 ?>
