c4854622eeae5fc618649390b042c14da6e1f3f4
Gabriel Fernandez <gabofer82@gmail.com>
2011-12-04 11:52:18 -0200

    contenido nuevo

---
+++ lang/es.php
@@ -0,0 +1,9 @@
+<?php
+    $saludo_bar = 'Bienvenido';
+    $usr_bar = 'Usuario';
+    $pass_bar = 'Password';
+    $submit_bar = 'Entrar';
+    $reglink_bar= 'Registrarse';
+    $logout_bar = 'Salir';
+	$visit_bar = 'Visitante';
+?>
